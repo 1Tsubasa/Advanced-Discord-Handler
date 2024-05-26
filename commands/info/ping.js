750fbd02e64d82c85ad9ff236df2efffe407edbb
Tsu <126349790+1Tsubasa@users.noreply.github.com>
2024-05-26 20:05:16 +0200

    Update ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -10,10 +10,31 @@ const {
     usage: '',
     description: "Gives you information on how fast the Bot can respond to you",
     category: "info",
-    cooldown: 10,
-    userPermissions: "",
-    botPermissions: "",
+    cooldown: 5000,
+    userPermissions: [],
+    botPermissions: [],
     ownerOnly: false,
     toggleOff: false,
-    
+    topGgOnly: false,
+    bumpOnly: false,
+    guildOwnerOnly: false,
+    run: async(client, message) => {
+      const embed = new MessageEmbed()
+    .addField("Ping", `Calcul en cours`, true)
+          .setTitle(`🏓 Ping!`)
+          .addField("\`Latence Bot\`", `${client.ws.ping}ms`, true)
+          .setFooter(client.footer.text, client.footer.icon_url)
+          .setThumbnail(client.thumbnail.icon)
+          .setColor(client.color);
+      const msg = await message.reply({ embeds: [embed], allowedMentions: { repliedUser: false } });
+      const embed2 = new MessageEmbed()
+          .setTitle(`🏓 Pong!`)
+          .addField("\`API Discord\`", `${msg.createdAt - message.createdAt + "ms"}`)
+          .addField("\`Latence Bot\`", `${client.ws.ping}ms`)
+          .setFooter(client.footer.text, client.footer.icon_url)
+          .setThumbnail(client.thumbnail.icon)
+          .setColor(client.color);
+      return msg.edit({ embeds: [embed2] })
+
+    }
   }
